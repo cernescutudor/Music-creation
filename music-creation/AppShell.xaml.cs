dd975aaf85a4ad71e4c77e45ea5c3dcfa8059939
Cseke <eu@lamine.com>
2024-04-12 12:37:07 +0300

    new project

---
+++ music-creation/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace music_creation;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ music-creation/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace music_creation;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
